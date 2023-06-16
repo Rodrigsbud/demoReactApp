e17dd1aa08c7e3dfcdb06918fb6708bb20da57e0
Rodrigo <quishpi444@gmail.com>
2023-06-15 19:06:11 -0500

    click

--- src/componentes/NavigationBar.js
+++ src/componentes/NavigationBar.js
@@ -1,15 +1,28 @@
-import React,{useState} from 'react'
-import { link } from 'react-router-dom';
+import React,{useState} from 'react';
+import { Link } from 'react-router-dom';
 
 function NavigationBar() {
+    const [click, setClick]= useState(false);
+
+    const handleClick = () => setClick(!click);
   return (
     <>
     <nav className="navbar">
         <div className='navbar-container'>
-            <Link to="/" className="navbar-logo">
-                TRVL <i className='fab fa-typo3' />
-
+            <Link to='/' className='navbar-logo'>
+            Gaming <i className='fab fa-typo3' />
             </Link>
+            <div className='menu-icon'onClick={handleClick}>
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <li className='nav-item'>
+
+                </li>
+            </ul>
+                
+
+            
         </div>
     </nav>
     
